eb33363ccd0ed6cb14249dd824ce084b7078e622
eliglezz <eliglezz@users.noreply.github.com>
2022-04-24 01:48:53 -0700

    removed empty about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -6,8 +6,8 @@ export default function AboutMe() {
             <div className="left">
                 <div className="wrapper">
                     <h1>Eliezer Gonzalez,</h1>
-                    <h3>Web Developer</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sapiente enim quaerat quibusdam molestias hic, ipsam quisquam nihil laboriosam molestiae expedita vero, aperiam nesciunt impedit soluta non? Totam, aut eaque.</p>
+                    <h3>Full Stack Web Developer</h3>
+                    {/* <p>Removed about me for now</p> */}
                 </div>
             </div>
         </div>
